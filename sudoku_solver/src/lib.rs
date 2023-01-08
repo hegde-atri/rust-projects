5618ae6e672416da40c28fca30b7f9ba5798d749
hegde-atri <iamatrihegde@outlook.com>
2023-01-08 11:50:11 +0000

    Initialized project structure + added unit tests

---
+++ sudoku_solver/src/lib.rs
@@ -0,0 +1,20 @@
+pub struct Solver;
+
+impl Solver {
+    pub fn solve(board: [[u8; 9]; 9]) -> [[u8; 9]; 9] {
+        // TODO
+        return [[0u8; 9]; 9];
+    }
+
+    pub fn verbose() {
+        // TODO: display each step taken?
+    }
+
+    pub fn benchmark() {
+        // TODO: display time taken, and total steps taken?
+    }
+}
+
+pub fn print_board(board: [[u8; 9]; 9]) {
+    // TODO
+}
